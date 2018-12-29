3f205c45dc408ecafbd4f4cad12c6627c6cab2e2
Gerald <gerald.ramier@gmail.com>
2018-12-29 15:25:24 +0100

    *update config *removed jwt json file *added generate all command

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,29 @@ var fs = require('fs');
 var nJwt = require('njwt');
 
 function generateJwt(cb) {
-  var signingKey = fs.readFileSync('output/server.key');
-  var claims = JSON.parse(fs.readFileSync('config/jwt.config.json'));
+  var signingKey = fs.readFileSync(process.env.npm_package_config_pkey_path);
+  var claims = {
+    "iss" : process.env.npm_package_config_jwt_iss,
+    "aud" : process.env.npm_package_config_jwt_aud,
+    "sub" : process.env.npm_package_config_jwt_sub,
+  };
 
-  var jwt = nJwt.create(claims, signingKey, "RS256");
+  var jwt = nJwt.create(claims, signingKey,process.env.npm_package_config_jwt_alg);
   jwt.setNotBefore();
-
   var token = jwt.compact();
-  console.log(process.argv);
+  fs.writeFileSync(process.env.npm_package_config_jwt_path, token);
   console.log(token);
-  fs.writeFileSync('output/token.jwt', token);
   cb();
 }
 
-function generatePrivateKey(cb) {
-  openssl.generateRSAPrivateKey({}, function (err, key, cmd) {
-    console.log(cmd);
-    console.log(key);
-    fs.writeFile("./private.key", key, {});
-  });
-  cb();
-}
+// function generatePrivateKey(cb) {
+//   openssl.generateRSAPrivateKey({}, function (err, key, cmd) {
+//     console.log(cmd);
+//     console.log(key);
+//     fs.writeFile("./private.key", key, {});
+//   });
+//   cb();
+// }
 
-exports.generatePrivateKey = generatePrivateKey;
+// exports.generatePrivateKey = generatePrivateKey;
 exports.generateJwt = generateJwt;
